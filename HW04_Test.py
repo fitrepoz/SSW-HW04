68a0895b371b1e64b6b577922ddceb83279f895d
Sadie Stokes <sastokes@liberty.edu>
2020-03-06 17:55:09 -0500

    hw04

--- HW04_Test.py
+++ HW04_Test.py
@@ -8,4 +8,4 @@ class TestCase(unittest.TestCase):
         self.assertEqual(view_repos("fitrepoz"), {'810A': 1, 'http---code.google.com-p-sharpdx-': 2, 'projectxenie': 3, 'sastokes.github.io': 4, 'sharpdx': 2, 'SSW-567': 30, 'SSW-HW02': 3, 'SSW-HW04': 13, 'SSW810GHrepo': 5, 'SSWTeamD': 2})
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
