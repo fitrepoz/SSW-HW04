78c5192e12538815333e17a486bc0cc2cd97cbc6
fitrepoz <sastokes@liberty.edu>
2020-03-06 11:58:30 -0500

    Update HW04_Test.py

--- HW04_Test.py
+++ HW04_Test.py
@@ -1,18 +1,20 @@
 import unittest
 
-from HW04 import view_repos
+from HW04 import view_github_repos
 
 class TestHW(unittest.TestCase):
 
     def test_github_api(self):
-        self.assertEqual(view_repos('fitrepoz'),['Repo:810A Number of commits: 1',
-        'Repo:http---code.google.com-p-sharpdx- Number of commits: 2', 
-        'Repo:projectxenie Number of commits: 3', 
-        'Repo:sastokes.github.io Number of commits: 4', 
-        'Repo:sharpdx Number of commits: 2', 
-        'Repo:SSW-567 Number of commits: 30', 
-        'Repo:SSW810GHrepo Number of commits: 5', 
-        'Repo:SSWTeamD Number of commits: 2'])
+        self.assertEqual(view_github_repos('fitrepoz'),['Repo:810A Number of commits: 1',
+   'Repo:http---code.google.com-p-sharpdx- Number of commits: 2',
+   'Repo:projectxenie Number of commits: 3',
+   'Repo:sastokes.github.io Number of commits: 4',
+   'Repo:sharpdx Number of commits: 2',
+   'Repo:SSW-567 Number of commits: 30',
+-  'Repo:SSW-HW02 Number of commits: 2',
+-  'Repo:SSW-HW04 Number of commits: 7',
+   'Repo:SSW810GHrepo Number of commits: 5',
+   'Repo:SSWTeamD Number of commits: 2'])
 
 
 if __name__ == '__main__':
